b2a4abfeb0e387062a2953043ae00d2e51a03554
CdRGit <github@catelyn.tech>
2023-11-30 22:42:30 +0100

    added file reading code

--- template/src/main.c
+++ template/src/main.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#include <stdint.h>
+uint8_t* read_data(const char* const fileName) {
+	uint8_t* data;
+
+	printf("reading: %s\n", fileName);
+
+	int fd = open(fileName, O_RDONLY);
+
+	struct stat sb;
+
+	fstat(fd, &sb);
+	printf("Size: %lu\n", (uint64_t)sb.st_size);
+
+	data = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (data == MAP_FAILED) {
+		fprintf(stderr, "Could not memory map file: %s\n", fileName);
+		exit(1);
+	}
+
+	return data;
+}
+
 const bool TRY_REAL_INPUT = false;
 
-const char test_input[] = "day1/test.txt";
-const char real_input[] = "day1/real.txt";
+const char test_input[] = "test_input/day1.txt";
+const char real_input[] = "real_input/day1.txt";
 
-void part_1(const char* path);
-void part_2(const char* path);
+void part_1(const char* path, const uint8_t* data);
+void part_2(const char* path, const uint8_t* data);
 
 int main(int argc, const char** argv) {
-	part_1(test_input);
-	if (TRY_REAL_INPUT)
-		part_1(real_input);
+	uint8_t* test_data = read_data(test_input);
 
-	part_2(test_input);
-	if (TRY_REAL_INPUT)
-		part_2(real_input);
+	part_1(test_input, test_data);
+	part_2(test_input, test_data);
+
+	if (TRY_REAL_INPUT) {
+		uint8_t* real_data = read_data(real_input);
+		part_1(real_input, real_data);
+		part_2(real_input, real_data);
+	}
 }
 
-void part_1(const char* path) {
+void part_1(const char* path, const uint8_t* data) {
 	printf("part 1: %s\n", path);
 }
-void part_2(const char* path) {
+void part_2(const char* path, const uint8_t* data) {
 	printf("part 2: %s\n", path);
 }
